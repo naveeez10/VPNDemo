e26dbdf1174ea98d729053f3b90a0aa2d6477310
Naviz <naveezkhoja1063@gmail.com>
2025-05-12 17:13:37 +0530

    initial commit

---
+++ FilterDataProvider/FilterControlProvider.swift
@@ -0,0 +1,26 @@
+//
+//  FilterControlProvider.swift
+//  FilterDataProvider
+//
+//  Created by Apple on 12/05/25.
+//
+
+import NetworkExtension
+
+class FilterControlProvider: NEFilterControlProvider {
+
+    override func startFilter(completionHandler: @escaping (Error?) -> Void) {
+        // Add code to initialize the filter
+        completionHandler(nil)
+    }
+    
+    override func stopFilter(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
+        // Add code to clean up filter resources
+        completionHandler()
+    }
+    
+    override func handleNewFlow(_ flow: NEFilterFlow, completionHandler: @escaping (NEFilterControlVerdict) -> Void) {
+        // Add code to determine if the flow should be dropped or not, downloading new rules if required
+        completionHandler(.allow(withUpdateRules: false))
+    }
+}
